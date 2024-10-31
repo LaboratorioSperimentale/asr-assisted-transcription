6e809a97eb659c5c752e32e0bd31195b64f7c50c
martinasimonotti <simonottimartina@gmail.com>
2024-10-31 13:02:45 +0100

    prova

--- src/asr_analysis/main.py
+++ src/asr_analysis/main.py
@@ -14,6 +14,7 @@ with open("data/csv_puliti/01_ParlaBOA_E.csv", encoding="utf-8") as fin:
 
 print(transcript)
 
+i 
 # DONE: read transcript from csv
 # DONE: remove spaces (see init function --- done)
 
