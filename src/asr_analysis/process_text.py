2c43f8d29e5f763fd10c5aab77d200374fb617e9
martinasimonotti <simonottimartina@gmail.com>
2024-11-18 13:59:09 +0100

    parentheses check

--- src/asr_analysis/process_text.py
+++ src/asr_analysis/process_text.py
@@ -24,7 +24,7 @@ def remove_spaces(transcription):
 
 # transform "pò" into "po'" (keep count)
 
-def replace_pò(transcription):
+def replace_po(transcription):
     tot_subs = 0
     new_string, subs_made = re.subn(r"\bpò\b", "po'", transcription)
 
@@ -34,23 +34,25 @@ def replace_pò(transcription):
 
     return tot_subs, transcription.strip()
 
-# transform "perchè" into "perché" (keep count)
+# transform "chè" into "ché" (keep count)
 
-def replace_perchè(transcription):
+def replace_che(transcription):
     tot_subs = 0
-    new_string, subs_made = re.subn(r"\bperchè\b", "perché", transcription)
-
+    regex = r"\b(perchè|benchè|finchè|poiché|anziché|dopodiché|granché|fuorché|affinché|pressoché)\b"
+    # chè > ché
+    new_string, subs_made = re.subn(regex, lambda m: m.group(0).replace("chè", "ché"), transcription)
+    
     if subs_made > 0:
         tot_subs += subs_made
         transcription = new_string
-
+    
     return tot_subs, transcription.strip()
 
 # remove initial and final pauses (keep count)
 
 def remove_pauses(transcription):
     tot_subs = 0
-    new_string, subs_made = re.subn(r"^\(.\)\s*|\s*\(.\)$", "", transcription)
+    new_string, subs_made = re.subn(r"^\(\.\)\s*|\s*\(\.\)$", "", transcription)
 
     if subs_made > 0:
         tot_subs += subs_made
@@ -71,16 +73,49 @@ def clean_non_jefferson_symbols(transcription):
     return tot_subs, transcription.strip()
 
 
-# 	def check_normal_parentheses(self, open_char, close_char):
-# 		is_open = False
+# correct unbalanced °° (must be even)
+def check_even_dots(transcription):
+      even_dots_count = transcription.count ("°")
+    
+      if even_dots_count % 2 == 0:
+          return "Balanced"
+      else:
+          return "Not balanced"
+      
+  
+# correct unbalanced parentheses (keep count)
 
-# 		for char in self.annotation:
-# 			if char == open_char:
-# 				if is_open:
-# 					self.errors["UNBALANCED_PARENTHESES"] += 1
-# 					return
-# 				else:
-# 					is_open = True
+def check_parentheses(self):
+    for annotation in self.transcription_units:
+        annotation.check_normal_parentheses("(", ")") # checks that each open round bracket has a corresponding closed round bracket
+        annotation.check_normal_parentheses("[", "]") # checks that each open square bracket has a corresponding closed square bracket
+        annotation.check_angular_parentheses("<", ">") 
+        annotation.check_angular_parentheses(">", "<") 
+
+def check_normal_parentheses(annotation, open_char, close_char):
+    count = 0
+    for char in annotation:
+        if char == open_char:
+            count += 1
+        elif char == close_char:
+            count -= 1
+            if count < 0:
+                return "Not balanced"
+    
+    if count == 0:
+        return "Balanced"
+    else:
+        return "Not balanced"
+
+# def check_normal_parentheses(self, open_char, close_char):
+ #		is_open = False
+   # for char in self.annotation:
+	# if char == open_char:
+ 		#	if is_open:
+ 		#		self.errors["UNBALANCED_PARENTHESES"] += 1
+# return
+ #   else:
+ 	#		    is_open = True
 
 # 			if char == close_char:
 # 				if not is_open:
@@ -91,6 +126,29 @@ def clean_non_jefferson_symbols(transcription):
 
 # 		if is_open:
 # 			self.errors["UNBALANCED_PARENTHESES"] += 1
+
+
+def check_angular_parentheses(annotation):
+    count = 0
+    for char in annotation:
+    # increment count if the character is "<"
+        if char == "<":
+            count += 1  
+        
+        # decrement count if the character is ">"
+        elif char == ">":
+            count -= 1  
+            
+            # if the count is negative, parentheses are not balances
+            if count < 0:
+                return "Not balanced"
+    
+  # if count is zero, there are as many "<" as ">"
+    
+    if count == 0:
+        return "Balanced"
+    else:
+        return "Not balanced"
 
 # 	def check_angular_parentheses(self):
 # 		openlist = {"left": False,
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -11,8 +11,33 @@ def test_meta_tag():
     assert pt.meta_tag("(.) ciao") == "{PAUSE} ciao"
     assert pt.meta_tag("ciao ((bla bla)) ciao") == "ciao {bla_bla} ciao"
 
-def test_replace_pò():
-    assert pt.replace_pò("pò") 
+def test_replace_po():
+    assert pt.replace_pò("po") 
 
-def test_replace_perchè():
-    assert pt.replace_perchè("perchè") 
+def test_replace_che():
+    assert pt.replace_che("perchè") == (1, "perché")
+    assert pt.replace_che("finchè") == (1, "finché")
+
+def test_pauses():
+    assert pt.remove_pauses("(.) ciao") == (1, "ciao")
+    assert pt.remove_pauses("ciao (.)") == (1, "ciao")
+    assert pt.remove_pauses("(.) ciao (.)") == (2, "ciao")
+    assert pt.remove_pauses("ciao (.) ciao") == (0, "ciao (.) ciao")
+    assert pt.remove_pauses("(a) casa") == (0, "(a) casa")
+
+def test_check_even_dots():
+    assert pt.check_even_dots("°ciao°") == ("Balanced")
+    assert pt.check_even_dots("°ciao") == ("Not balanced")
+
+def test_check_normal_parentheses():
+    assert pt.check_normal_parentheses("(ciao)", "(", ")") == ("Balanced")
+    assert pt.check_normal_parentheses("(ciao", "(", ")") == ("Not balanced")
+    assert pt.check_normal_parentheses("[ciao]", "[", "]") == ("Balanced")
+    assert pt.check_normal_parentheses("ciao]", "[", "]") == ("Not balanced")
+
+def test_check_angular_parentheses():
+    assert pt.check_angular_parentheses("<ciao>") == ("Balanced")
+    assert pt.check_angular_parentheses(">ciao<") == ("Balanced") 
+    assert pt.check_angular_parentheses("<ciao") == ("Not balanced")
+    assert pt.check_angular_parentheses("ciao>") == ("Not balanced")
+    assert pt.check_angular_parentheses("<<ciao>>") == ("Not balanced")
